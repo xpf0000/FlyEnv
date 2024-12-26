4773d7c5253917f7355cf68229b9b1d969f1a851
x <250881478@qq.com>
2024-12-26 20:00:45 +0800

    1. Fix Bug

---
+++ src/render/components/AppLog/btn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="inline-flex items-center justify-center p-1" @click.stop="showLog()">
+    <yb-icon :svg="import('@/svg/log1.svg?raw')" width="17" height="17" />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { AsyncComponentShow } from '@/util/AsyncComponent'
+
+  let LogVM: any
+  import('./log.vue').then((res) => {
+    LogVM = res.default
+  })
+  const showLog = () => {
+    AsyncComponentShow(LogVM).then()
+  }
+</script>
--- src/render/components/AppLog/log.vue
+++ src/render/components/AppLog/log.vue
@@ -5,6 +5,7 @@
     :destroy-on-close="true"
     :with-header="false"
     :close-on-click-modal="false"
+    @open="onOpen"
     @closed="closedFn"
   >
     <div class="host-vhost">
@@ -15,42 +16,103 @@
         </div>
       </div>
       <div class="main-wapper">
-        <LogVM ref="log" :log-file="filepath" />
+        <div ref="log" class="block"></div>
       </div>
-      <ToolVM :log="log" />
+      <div class="tool">
+        <el-tooltip :show-after="600" :content="I18nT('base.open')" placement="top">
+          <el-button
+            class="shrink0"
+            :disabled="AppLogStore.checkLogFile()"
+            @click="AppLogStore.open()"
+          >
+            <FolderOpened class="w-5 h-5 p-0.5" />
+          </el-button>
+        </el-tooltip>
+        <el-tooltip :show-after="600" :content="I18nT('base.clean')" placement="top">
+          <el-button
+            class="shrink0"
+            :disabled="AppLogStore.checkLogFile()"
+            @click="AppLogStore.clean()"
+          >
+            <Notebook class="w-5 h-5 p-0.5" />
+          </el-button>
+        </el-tooltip>
+      </div>
     </div>
   </el-drawer>
 </template>
 <script lang="ts" setup>
-  import { ref, computed } from 'vue'
+  import { ref, computed, onMounted, nextTick, onUnmounted, watch } from 'vue'
   import { I18nT } from '@shared/lang'
   import { AsyncComponentSetup } from '@/util/AsyncComponent'
-  import LogVM from '@/components/Log/index.vue'
-  import ToolVM from '@/components/Log/tool.vue'
-  import type { SoftInstalled } from '@/store/brew'
-
-  const { join } = require('path')
+  import { FolderOpened, Notebook } from '@element-plus/icons-vue'
+  import { AppLogStore } from '@/components/AppLog/store'
+  import { EditorConfigMake, EditorCreate } from '@/util/Editor'
+  import { editor } from 'monaco-editor/esm/vs/editor/editor.api.js'
 
   const { show, onClosed, onSubmit, closedFn } = AsyncComponentSetup()
 
-  const props = defineProps<{
-    type: string
-    version: SoftInstalled
-  }>()
-
   const log = ref()
 
-  const title = computed(() => {
-    return props.type === 'php-fpm-slow' ? I18nT('base.slowLog') : I18nT('php.fpmLog')
+  const logs = computed(() => {
+    return AppLogStore.log.join('\n')
   })
 
-  const filepath = computed(() => {
-    return join(global.Server.PhpDir, `${props.version.num}`, `var/log/${props.type}.log`)
-  })
+  let isBottom = false
+  let monacoInstance: editor.IStandaloneCodeEditor | null
+  const initEditor = () => {
+    if (!monacoInstance) {
+      const inputDom: HTMLElement = log.value as any
+      if (!inputDom || !inputDom?.style) {
+        return
+      }
+      monacoInstance = EditorCreate(inputDom, EditorConfigMake(logs.value, true, 'on'))
+      monacoInstance.onDidScrollChange(() => {
+        const scrollTop = monacoInstance!.getScrollTop()
+        const contentHeight = monacoInstance!.getContentHeight()
+        const viewportHeight = monacoInstance!.getLayoutInfo().height
+
+        // 判断是否滚动到底部
+        const isScrolledToBottom = scrollTop + viewportHeight >= contentHeight
+
+        if (isScrolledToBottom) {
+          // 已滚动到底部的处理逻辑
+          console.log('已滚动到底部')
+          isBottom = true
+        } else {
+          isBottom = false
+        }
+      })
+    } else {
+      monacoInstance.setValue(logs.value)
+    }
+  }
+
+  const onOpen = () => {
+    AppLogStore.index += 1
+  }
 
   const close = () => {
     show.value = false
   }
+
+  watch(logs, () => {
+    initEditor()
+    if (isBottom) {
+      monacoInstance?.setScrollTop(99999999)
+    }
+  })
+
+  onMounted(() => {
+    nextTick().then(() => {
+      initEditor()
+    })
+  })
+
+  onUnmounted(() => {
+    monacoInstance && monacoInstance.dispose()
+    monacoInstance = null
+  })
 
   defineExpose({
     show,
