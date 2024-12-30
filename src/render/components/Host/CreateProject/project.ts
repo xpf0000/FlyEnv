2487b0a016da3cb5550e73f6ea7d6d66bcf736b3
x <250881478@qq.com>
2024-12-30 23:43:34 +0800

    1. Fix Bug

--- src/render/components/Host/CreateProject/project.ts
+++ src/render/components/Host/CreateProject/project.ts
@@ -6,8 +6,12 @@ export const ProjectSetup = reactive<{
   tab: ProjectTypes
   collapse: Partial<Record<ProjectTypes, string[]>>
   search: Partial<Record<ProjectTypes, string>>
+  running: Partial<Record<ProjectTypes, boolean>>
+  log: Partial<Record<ProjectTypes, string[]>>
 }>({
   tab: 'PHP',
   collapse: {},
-  search: {}
+  search: {},
+  running: {},
+  log: {}
 })
