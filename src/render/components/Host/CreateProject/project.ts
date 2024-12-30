f0f13dbd16169d512bd1065a4fe4f007bb495261
x <250881478@qq.com>
2024-12-30 23:58:23 +0800

    1. Fix Bug

--- src/render/components/Host/CreateProject/project.ts
+++ src/render/components/Host/CreateProject/project.ts
@@ -2,16 +2,34 @@ import { reactive } from 'vue'
 
 export type ProjectTypes = 'PHP' | 'NodeJS'
 
+export type ProjectPHPForm = {
+  dir: string
+  php: string
+  composer: string
+  version: string | undefined
+}
+
 export const ProjectSetup = reactive<{
   tab: ProjectTypes
   collapse: Partial<Record<ProjectTypes, string[]>>
   search: Partial<Record<ProjectTypes, string>>
   running: Partial<Record<ProjectTypes, boolean>>
   log: Partial<Record<ProjectTypes, string[]>>
+  form: {
+    PHP: ProjectPHPForm
+  }
 }>({
   tab: 'PHP',
   collapse: {},
   search: {},
   running: {},
-  log: {}
+  log: {},
+  form: {
+    PHP: {
+      dir: '',
+      php: '',
+      composer: '',
+      version: undefined
+    }
+  }
 })
