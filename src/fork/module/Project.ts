6b1a307c65873bd9574fdc9b0d513cbb0cfafcf4
x <250881478@qq.com>
2024-12-30 21:03:17 +0800

    1. Fix Bug

--- src/fork/module/Project.ts
+++ src/fork/module/Project.ts
@@ -11,19 +11,9 @@ class Manager extends Base {
     super()
   }
 
-  createProject(dir: string, phpBin: string, framework: string, version: SoftInstalled) {
+  createProject(dir: string, php: string, composer: string, framework: string, version: string) {
     return new ForkPromise(async (resolve, reject, on) => {
       const cacheDir = global.Server.Cache
-      const binDir = join(global.Server.Cache!, 'bin')
-      await mkdirp(binDir)
-      const binPhp = join(binDir, 'php')
-      if (existsSync(binPhp)) {
-        await remove(binPhp)
-      }
-      const command = `echo '${global.Server.Password}' | sudo -S ln -s "${phpBin}" "${binPhp}"`
-      try {
-        await execPromise(command)
-      } catch (e) {}
       if (framework === 'wordpress') {
         const tmpl = `{
   "require": {
@@ -36,17 +26,29 @@ class Manager extends Base {
   }
 }
 `
-        const sh = join(global.Server.Static!, 'sh/project-new-wordpress.sh')
-        const copyfile = join(global.Server.Cache!, 'project-new-wordpress.sh')
-        if (existsSync(copyfile)) {
-          await unlink(copyfile)
-        }
         await writeFile(join(dir, 'composer.json'), tmpl)
-        await copyFile(sh, copyfile)
-        await chmod(copyfile, '0777')
-        const params = [copyfile, cacheDir, dir, binDir]
+
+        const command = ['@echo off', 'chcp 65001>nul', `cd /d "${dir}"`]
+        if (php && composer) {
+          command.push(`"${php}" "${composer}" update`)
+        } else if (php) {
+          command.push(`"${php}" composer update`)
+        } else if (composer) {
+          command.push(`php "${composer}" update`)
+        } else {
+          command.push(`php composer update`)
+        }
+        const copyfile = join(global.Server.Cache!, `${uuid()}.cmd`)
+        console.log('createProject copyfile: ', copyfile)
+        await writeFile(copyfile, command.join('\n'))
+        const params = [copyfile]
         console.log('params: ', params.join(' '))
-        spawnPromise('zsh', params).on(on).then(resolve).catch(reject)
+        spawnPromise(`${basename(copyfile)}`, [], {
+          cwd: global.Server.Cache!
+        })
+          .on(on)
+          .then(resolve)
+          .catch(reject)
       } else {
         const names: { [k: string]: string } = {
           laravel: 'laravel/laravel',
