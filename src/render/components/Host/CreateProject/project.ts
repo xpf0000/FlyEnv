bd38223e8baa878efe22d3b98e4a17d3df005954
x <250881478@qq.com>
2024-12-31 00:18:39 +0800

    1. Fix Bug

--- src/render/components/Host/CreateProject/project.ts
+++ src/render/components/Host/CreateProject/project.ts
@@ -7,29 +7,43 @@ export type ProjectPHPForm = {
   php: string
   composer: string
   version: string | undefined
+  frameWork: string
+  running: boolean
+  created: boolean
 }
 
 export const ProjectSetup = reactive<{
   tab: ProjectTypes
   collapse: Partial<Record<ProjectTypes, string[]>>
   search: Partial<Record<ProjectTypes, string>>
-  running: Partial<Record<ProjectTypes, boolean>>
   log: Partial<Record<ProjectTypes, string[]>>
   form: {
     PHP: ProjectPHPForm
   }
+  phpFormInit: () => void
 }>({
   tab: 'PHP',
   collapse: {},
   search: {},
-  running: {},
   log: {},
   form: {
     PHP: {
       dir: '',
       php: '',
       composer: '',
-      version: undefined
+      version: undefined,
+      frameWork: '',
+      running: false,
+      created: false
     }
+  },
+  phpFormInit() {
+    this.form.PHP.dir = ''
+    this.form.PHP.php = ''
+    this.form.PHP.composer = ''
+    this.form.PHP.version = undefined
+    this.form.PHP.frameWork = ''
+    this.form.PHP.created = false
+    this.form.PHP.running = false
   }
 })
