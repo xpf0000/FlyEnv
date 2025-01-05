2a6435873c22aee428070d758943a11221b0f5ce
x <250881478@qq.com>
2025-01-05 11:06:57 +0800

    1. Fix Bug

--- src/fork/module/host/Apache.ts
+++ src/fork/module/host/Apache.ts
@@ -1,7 +1,7 @@
 import type { AppHost } from '@shared/app'
 import { join } from 'path'
-import { mkdirp, readFile, writeFile } from 'fs-extra'
-import { hostAlias, execPromiseRoot } from '../../Fn'
+import { copyFile, mkdirp, readFile, remove, writeFile } from 'fs-extra'
+import { hostAlias } from '../../Fn'
 import { vhostTmpl } from './Host'
 import { existsSync } from 'fs'
 import { isEqual } from 'lodash'
@@ -105,11 +105,8 @@ export const updateApacheConf = async (host: AppHost, old: AppHost) => {
     const arr = [avhost, accesslogap, errorlogap]
     for (const f of arr) {
       if (existsSync(f.oldFile)) {
-        await execPromiseRoot(`copy -Force ${f.oldFile} ${f.newFile}`)
-        await execPromiseRoot(`del -Force ${f.oldFile}`)
-      }
-      if (existsSync(f.newFile)) {
-        await execPromiseRoot(`icacls ${f.newFile} /grant Everyone:F`)
+        await copyFile(f.oldFile, f.newFile)
+        await remove(f.oldFile)
       }
     }
   }
--- src/fork/module/host/Nginx.ts
+++ src/fork/module/host/Nginx.ts
@@ -1,7 +1,7 @@
 import type { AppHost } from '@shared/app'
-import { dirname, join } from 'path'
-import { mkdirp, readFile, writeFile } from 'fs-extra'
-import { hostAlias, execPromiseRoot } from '../../Fn'
+import { basename, dirname, join } from 'path'
+import { copyFile, mkdirp, readFile, remove, writeFile } from 'fs-extra'
+import { hostAlias } from '../../Fn'
 import { vhostTmpl } from './Host'
 import { existsSync } from 'fs'
 import { isEqual } from 'lodash'
@@ -187,11 +187,8 @@ export const updateNginxConf = async (host: AppHost, old: AppHost) => {
     const arr = [nvhost, rewritep, accesslogng, errorlogng]
     for (const f of arr) {
       if (existsSync(f.oldFile)) {
-        await execPromiseRoot(`copy -Force ${f.oldFile} ${f.newFile}`)
-        await execPromiseRoot(`del -Force ${f.oldFile}`)
-      }
-      if (existsSync(f.newFile)) {
-        await execPromiseRoot(`icacls ${f.newFile} /grant Everyone:F`)
+        await copyFile(f.oldFile, f.newFile)
+        await remove(f.oldFile)
       }
     }
   }
