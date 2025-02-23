034b7cab3474d678b15c38a929254a7453827a04
x <250881478@qq.com>
2025-02-23 16:47:53 +0800

    1.Add FlyEnv NodeJS manager module. 2.Add Ruby module 3.Add Elasticsearch module

--- src/render/components/PHP/aside.vue
+++ src/render/components/PHP/aside.vue
@@ -42,9 +42,13 @@
     set(v: boolean) {
       const all: Array<Promise<any>> = []
       if (v) {
+        const runNum: Set<number> = new Set<number>()
         phpVersions?.value?.forEach((v) => {
           if (v?.version && appStore.phpGroupStart?.[v.bin] !== false && !v?.run) {
-            all.push(startService('php', v))
+            if (!runNum.has(v.num!)) {
+              runNum.add(v.num!)
+              all.push(startService('php', v))
+            }
           }
         })
       } else {
@@ -89,9 +93,13 @@
       }
     } else {
       if (showItem?.value) {
+        const runNum: Set<number> = new Set<number>()
         phpVersions?.value?.forEach((v) => {
           if (v?.version && appStore.phpGroupStart?.[v.bin] !== false && !v?.run) {
-            all.push(startService('php', v))
+            if (!runNum.has(v.num!)) {
+              runNum.add(v.num!)
+              all.push(startService('php', v))
+            }
           }
         })
       }
