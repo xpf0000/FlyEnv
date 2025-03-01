752c487e329744a68c2d8cbc4f76181c9a89bcbe
x <250881478@qq.com>
2025-03-01 22:42:37 +0800

    1.Add Ollama Module

--- src/render/components/AI/Prompt/setup.ts
+++ src/render/components/AI/Prompt/setup.ts
@@ -1,4 +1,12 @@
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue'
+import IPC from '@/util/IPC'
+import { StorageGet, StorageSet } from '@/util/Storage'
+
+export type LangItem = {
+  id: string
+  lang: string
+  name: string
+}
 
 export type PromptItem = {
   id: string
@@ -9,12 +17,61 @@ export type PromptItem = {
 
 export const PromptSetup = reactive<{
   lang: string
-  list: PromptItem[]
+  langs: LangItem[]
+  prompts: PromptItem[]
 }>({
   lang: '',
-  list: []
+  langs: [],
+  prompts: []
 })
 
 export const Setup = () => {
-  return {}
+  const fetchLangs = () => {
+    if (PromptSetup.langs.length > 0) {
+      return
+    }
+    const cache = StorageGet('fetchVerion-ai-lang')
+    if (cache) {
+      PromptSetup.langs = reactive(cache)
+      return
+    }
+    IPC.send('app-fork:ai', 'allLang').then((key: string, res: any) => {
+      IPC.off(key)
+      const list = res?.data ?? []
+      PromptSetup.langs = reactive(list)
+      if (list.length > 0) {
+        StorageSet('fetchVerion-ai-lang', list, 12 * 60 * 60)
+      }
+    })
+  }
+
+  const fetchPrompts = () => {
+    if (PromptSetup.prompts.length > 0) {
+      return
+    }
+    const cache = StorageGet('fetchVerion-ai-prompt')
+    if (cache) {
+      PromptSetup.prompts = reactive(cache)
+      return
+    }
+    IPC.send('app-fork:ai', 'allPrompt').then((key: string, res: any) => {
+      IPC.off(key)
+      const list = res?.data ?? []
+      PromptSetup.prompts = reactive(list)
+      if (list.length > 0) {
+        StorageSet('fetchVerion-ai-prompt', list, 12 * 60 * 60)
+      }
+    })
+  }
+
+  fetchLangs()
+  fetchPrompts()
+
+  const promptList = computed(() => {
+    return PromptSetup.prompts.filter((p) => p.lang === PromptSetup.lang)
+  })
+
+  return {
+    promptList
+  }
 }
