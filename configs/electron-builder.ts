e69d47564366e9b92525d517932c6a2a3ce62969
x <250881478@qq.com>
2025-03-05 23:48:21 +0800

    1.Add Ollama module. 2.Add AI Chat module

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import type { Configuration } from 'electron-builder'
 const conf: Configuration = {
   productName: 'FlyEnv',
   executableName: 'PhpWebStudy',
-  buildVersion: '4.8.6',
+  buildVersion: '4.8.7',
   electronVersion: '30.2.0',
   appId: 'phpstudy.xpfme.com',
   asar: true,
