b1a6e086b34bf7406924ea02545056ad8a299303
timint <info@tim-international.net>
2025-04-01 02:41:54 +0200

    + Swedish language

---
+++ src/lang/sv/index.ts
@@ -0,0 +1,61 @@
+import aiSV from './ai.json'
+import apacheSV from './apache.json'
+import appLogSV from './appLog.json'
+import asideSV from './aside.json'
+import baseSV from './base.json'
+import confSV from './conf.json'
+import feedbackSV from './feedback.json'
+import forkSV from './fork.json'
+import hostSV from './host.json'
+import mailpitSV from './mailpit.json'
+import menuSV from './menu.json'
+import mysqlSV from './mysql.json'
+import nginxSV from './nginx.json'
+import nodejsSV from './nodejs.json'
+import ollamaSV from './ollama.json'
+import phpSV from './php.json'
+import promptSV from './prompt.json'
+import redisSV from './redis.json'
+import serviceSV from './service.json'
+import setupSV from './setup.json'
+import tokenGeneratorSV from './token-generator.json'
+import toolsSV from './tools.json'
+import toolTypeSV from './toolType.json'
+import traySV from './tray.json'
+import updateSV from './update.json'
+import utilSV from './util.json'
+import versionmanagerSV from './versionmanager.json'
+import licensesSV from './licenses.json'
+
+export default {
+  sv: {
+    licenses: licensesSV,
+    ai: aiSV,
+    apache: apacheSV,
+    appLog: appLogSV,
+    aside: asideSV,
+    base: baseSV,
+    conf: confSV,
+    feedback: feedbackSV,
+    fork: forkSV,
+    host: hostSV,
+    mailpit: mailpitSV,
+    menu: menuSV,
+    mysql: mysqlSV,
+    nginx: nginxSV,
+    nodejs: nodejsSV,
+    ollama: ollamaSV,
+    php: phpSV,
+    prompt: promptSV,
+    redis: redisSV,
+    service: serviceSV,
+    setup: setupSV,
+    'token-generator': tokenGeneratorSV,
+    tools: toolsSV,
+    toolType: toolTypeSV,
+    tray: traySV,
+    update: updateSV,
+    util: utilSV,
+    versionmanager: versionmanagerSV
+  }
+}
