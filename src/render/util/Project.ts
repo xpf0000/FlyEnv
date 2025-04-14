6b88806f5f800c09e4a373328317623d77791215
x <250881478@qq.com>
2025-04-15 00:15:51 +0800

    1. Add NodeJS Project List

---
+++ src/render/util/Project.ts
@@ -0,0 +1,22 @@
+import { MessageError, MessageSuccess } from '@/util/Element'
+import { I18nT } from '@lang/index'
+import IPC from '@/util/IPC'
+
+const { clipboard } = require('@electron/remote')
+export const Project = {
+  copyPath(dir: string) {
+    clipboard.writeText(dir)
+    MessageSuccess(I18nT('base.copySuccess'))
+  },
+  openPath(
+    dir: string,
+    flag: 'Terminal' | 'vscode' | 'vs' | 'PhpStorm' | 'WebStorm' | 'HBuilderX'
+  ) {
+    IPC.send('app-fork:tools', 'openPathByApp', dir, flag).then((key: string, res: any) => {
+      IPC.off(key)
+      if (res?.code === 1) {
+        MessageError(res?.msg ?? '')
+      }
+    })
+  }
+}
