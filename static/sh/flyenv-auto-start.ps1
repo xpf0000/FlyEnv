65dd9713f84ac3ffb868220d9a04504433eb9cd3
徐鹏飞 <250881478@qq.com>
2025-04-25 22:43:08 +0800

    1. Add NodeJS Project Manage

--- static/sh/flyenv-auto-start.ps1
+++ static/sh/flyenv-auto-start.ps1
@@ -1,31 +1,63 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-# 配置参数
-$taskName = "FlyEnvStartup"
-$exePath = "#EXECPATH#"
+try {
+    $taskName = "FlyEnvStartup"
+    $exePath = "#EXECPATH#"
 
-$action = New-ScheduledTaskAction -Execute "`"$exePath`""
-$trigger = New-ScheduledTaskTrigger -AtLogon
+    if (-not (Test-Path -LiteralPath $exePath)) {
+        throw "$exePath not exist"
+    }
 
-$settings = New-ScheduledTaskSettingsSet `
-    -DontStopOnIdleEnd:$true `
-    -StartWhenAvailable:$true `
-    -AllowStartIfOnBatteries:$true `
-    -DontStopIfGoingOnBatteries:$true `
-    -ExecutionTimeLimit "PT0S" `
-    -RunOnlyIfLoggedOn:$true
+    $xmlConfig = @"
+<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Description>FlyEnv Auto Start</Description>
+  </RegistrationInfo>
+  <Triggers>
+    <LogonTrigger>
+      <Enabled>true</Enabled>
+    </LogonTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>$(whoami)</UserId>
+      <LogonType>InteractiveToken</LogonType>
+      <RunLevel>HighestAvailable</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>"$exePath"</Command>
+    </Exec>
+  </Actions>
+</Task>
+"@
 
-Register-ScheduledTask `
-    -TaskName $taskName `
-    -Action $action `
-    -Trigger $trigger `
-    -Settings $settings `
-    -RunLevel Highest `
-    -Force
+    $xmlPath = "$env:TEMP\FlyEnvTask.xml"
+    $xmlConfig | Out-File -FilePath $xmlPath -Encoding Unicode -Force
 
-if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
-    Write-Host "Task Create Success: $taskName"
-} else {
+    schtasks /Create /XML "$xmlPath" /TN "$taskName" /F
+
+    if (Test-Path -LiteralPath $xmlPath) {
+        Remove-Item -LiteralPath $xmlPath -Force
+    }
+
+    if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+        Write-Host "Task Create Success: $taskName"
+    } else {
+        throw "Task Create Failed"
+    }
+    exit 0
+}
+catch {
     Write-Host "Task Create Failed"
     exit 1
 }
