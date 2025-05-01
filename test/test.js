dfbc7597d678d743b1372d1f22237e52cf982936
徐鹏飞 <250881478@qq.com>
2025-05-01 23:38:41 +0800

    1. Site add reverse proxy setting 2. php.ini add curl.cainfo and openssl.cafile setting 3. Tomcat add CATALINA_BASE setting

--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
-let num = 1
-const echo = () => {
-  console.log('num: ', num)
-  num += 1
-  setTimeout(echo, 2000)
-}
+const { exec } = require('child-process-promise')
 
-echo()
+const psName = 'start.ps1'
+
+exec(
+  `powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Unblock-File -LiteralPath './${psName}'; & './${psName}'"`,
+  {
+    cwd: `C:\\Users\\x\\Desktop\\Git Hub\\FlyEnv\\data\\server\\apache`
+  }
+)
+  .then((res) => {
+    console.log('res: ', res)
+  })
+  .catch((e) => {
+    console.log('e: ', e)
+  })
