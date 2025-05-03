81de83780f2a823f7effb91f860bb5df9f85ba77
bariskanberkay <berkaybariskan0208@gmail.com>
2025-05-04 00:34:06 +0300

    feat: added full Turkish language support to the application

---
+++ src/lang/tr/index.ts
@@ -0,0 +1,63 @@
+import aiTR from './ai.json'
+import apacheTR from './apache.json'
+import appLogTR from './appLog.json'
+import asideTR from './aside.json'
+import baseTR from './base.json'
+import confTR from './conf.json'
+import feedbackTR from './feedback.json'
+import forkTR from './fork.json'
+import hostTR from './host.json'
+import mailpitTR from './mailpit.json'
+import menuTR from './menu.json'
+import mysqlTR from './mysql.json'
+import nginxTR from './nginx.json'
+import nodejsTR from './nodejs.json'
+import ollamaTR from './ollama.json'
+import phpTR from './php.json'
+import promptTR from './prompt.json'
+import redisTR from './redis.json'
+import serviceTR from './service.json'
+import setupTR from './setup.json'
+import tokenGeneratorTR from './token-generator.json'
+import toolsTR from './tools.json'
+import toolTypeTR from './toolType.json'
+import trayTR from './tray.json'
+import updateTR from './update.json'
+import utilTR from './util.json'
+import versionmanagerTR from './versionmanager.json'
+import licensesTR from './licenses.json'
+import requestTimer from './requestTimer.json'
+
+export default {
+  tr: {
+    licenses: licensesTR,
+    ai: aiTR,
+    apache: apacheTR,
+    appLog: appLogTR,
+    aside: asideTR,
+    base: baseTR,
+    conf: confTR,
+    feedback: feedbackTR,
+    fork: forkTR,
+    host: hostTR,
+    mailpit: mailpitTR,
+    menu: menuTR,
+    mysql: mysqlTR,
+    nginx: nginxTR,
+    nodejs: nodejsTR,
+    ollama: ollamaTR,
+    php: phpTR,
+    prompt: promptTR,
+    redis: redisTR,
+    service: serviceTR,
+    setup: setupTR,
+    'token-generator': tokenGeneratorTR,
+    tools: toolsTR,
+    toolType: toolTypeTR,
+    tray: trayTR,
+    update: updateTR,
+    util: utilTR,
+    versionmanager: versionmanagerTR,
+    requestTimer
+  }
+}
