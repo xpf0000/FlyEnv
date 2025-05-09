f4cd496e01e9241a2cca91ce99f7b06b7a9dee5d
徐鹏飞 <250881478@qq.com>
2025-05-10 00:34:36 +0800

    1. Site add reverse proxy setting 2. php.ini add curl.cainfo and openssl.cafile setting 3. Tomcat add CATALINA_BASE setting

--- static/sh/path-get.ps1
+++ static/sh/path-get.ps1
@@ -1,10 +1,9 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $regPath = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 try {
-    $regKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey('LocalMachine', [Microsoft.Win32.RegistryView]::Default)
+    $regKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey('LocalMachine', [Microsoft.Win32.RegistryView]::Registry64)
     if ($regKey) {
-        $subKeyName = Split-Path -Path $regPath -NoQualifier
-        $subKey = $regKey.OpenSubKey($subKeyName, $false)
+        $subKey = $regKey.OpenSubKey($regPath, $false)
         if ($subKey) {
             $rawValue = $subKey.GetValue("Path", $null, [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
             $subKey.Close()
