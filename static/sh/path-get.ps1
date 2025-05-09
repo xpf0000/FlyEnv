8c347a2d2be08a283f0b38d70609575c10ead57d
徐鹏飞 <250881478@qq.com>
2025-05-10 00:27:48 +0800

    1. Site add reverse proxy setting 2. php.ini add curl.cainfo and openssl.cafile setting 3. Tomcat add CATALINA_BASE setting

--- static/sh/path-get.ps1
+++ static/sh/path-get.ps1
@@ -1,6 +1,25 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$rawPath = (Get-ItemProperty `
-    -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" `
-    -Name "Path"
-).Path
-Write-Host "##FlyEnv-PATH-GET$($rawPath)FlyEnv-PATH-GET##"
+$regPath = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+try {
+    $regKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey('LocalMachine', [Microsoft.Win32.RegistryView]::Default)
+    if ($regKey) {
+        $subKeyName = Split-Path -Path $regPath -NoQualifier
+        $subKey = $regKey.OpenSubKey($subKeyName, $false)
+        if ($subKey) {
+            $rawValue = $subKey.GetValue("Path", $null, [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+            $subKey.Close()
+	          $regKey.Close()
+	          Write-Host "##FlyEnv-PATH-GET"
+            $rawValue
+            Write-Host "FlyEnv-PATH-GET##"
+        } else {
+            $regKey.Close()
+            exit 1
+        }
+    } else {
+        exit 1
+    }
+}
+catch {
+	exit 1
+}
