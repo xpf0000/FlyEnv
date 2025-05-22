8c2d0c8f788793a198f5158d53d7269ae14f08d2
徐鹏飞 <250881478@qq.com>
2025-05-22 16:40:18 +0800

    reinit

--- web/components/Nodejs/CreateProject.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-card class="w-full h-full overflow-hidden p-3 app-base-el-card">
-    <Create />
-  </el-card>
-</template>
-
-<script lang="ts" setup>
-  import Create from '@web/components/Host/CreateProject/nodejs.vue'
-</script>
