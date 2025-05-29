53b360dd930728b70cb3c0746c0971bd203654fb
MEZIANE Soufiane <soufiane.meziane@rte-france.com>
2025-05-29 16:48:51 +0200

    1. Added support for user-defined modules for WIN branch

---
+++ src/lang/fr/index.ts
@@ -0,0 +1,66 @@
+import aiEN from './ai.json'
+import apacheEN from './apache.json'
+import appLogEN from './appLog.json'
+import asideEN from './aside.json'
+import baseEN from './base.json'
+import confEN from './conf.json'
+import feedbackEN from './feedback.json'
+import forkEN from './fork.json'
+import hostEN from './host.json'
+import mailpitEN from './mailpit.json'
+import menuEN from './menu.json'
+import mysqlEN from './mysql.json'
+import nginxEN from './nginx.json'
+import nodejsEN from './nodejs.json'
+import ollamaEN from './ollama.json'
+import phpEN from './php.json'
+import promptEN from './prompt.json'
+import redisEN from './redis.json'
+import serviceEN from './service.json'
+import setupEN from './setup.json'
+import tokenGeneratorEN from './token-generator.json'
+import toolsEN from './tools.json'
+import toolTypeEN from './toolType.json'
+import trayEN from './tray.json'
+import updateEN from './update.json'
+import utilEN from './util.json'
+import versionmanagerEN from './versionmanager.json'
+import licensesEN from './licenses.json'
+import requestTimer from './requestTimer.json'
+import meilisearch from './meilisearch.json'
+import minio from './minio.json'
+export default {
+  en: {
+    minio,
+    meilisearch,
+    licenses: licensesEN,
+    ai: aiEN,
+    apache: apacheEN,
+    appLog: appLogEN,
+    aside: asideEN,
+    base: baseEN,
+    conf: confEN,
+    feedback: feedbackEN,
+    fork: forkEN,
+    host: hostEN,
+    mailpit: mailpitEN,
+    menu: menuEN,
+    mysql: mysqlEN,
+    nginx: nginxEN,
+    nodejs: nodejsEN,
+    ollama: ollamaEN,
+    php: phpEN,
+    prompt: promptEN,
+    redis: redisEN,
+    service: serviceEN,
+    setup: setupEN,
+    'token-generator': tokenGeneratorEN,
+    tools: toolsEN,
+    toolType: toolTypeEN,
+    tray: trayEN,
+    update: updateEN,
+    util: utilEN,
+    versionmanager: versionmanagerEN,
+    requestTimer
+  }
+}
