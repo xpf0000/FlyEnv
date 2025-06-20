27bdb0bedb184af1968110b7c1b35451a9ead5cf
openapphub <openapphub@outlook.com>
2025-06-20 09:54:42 +0800

    feat(i18n): Add Japanese translations for various application components and enhance language support

---
+++ src/lang/ja/index.ts
@@ -0,0 +1,67 @@
+import aiJA from './ai.json'
+import apacheJA from './apache.json'
+import appLogJA from './appLog.json'
+import asideJA from './aside.json'
+import baseJA from './base.json'
+import confJA from './conf.json'
+import feedbackJA from './feedback.json'
+import forkJA from './fork.json'
+import hostJA from './host.json'
+import licensesJA from './licenses.json'
+import mailpitJA from './mailpit.json'
+import meilisearchJA from './meilisearch.json'
+import menuJA from './menu.json'
+import minioJA from './minio.json'
+import mysqlJA from './mysql.json'
+import nginxJA from './nginx.json'
+import nodejsJA from './nodejs.json'
+import ollamaJA from './ollama.json'
+import phpJA from './php.json'
+import promptJA from './prompt.json'
+import redisJA from './redis.json'
+import requestTimerJA from './requestTimer.json'
+import serviceJA from './service.json'
+import setupJA from './setup.json'
+import tokenGeneratorJA from './token-generator.json'
+import toolsJA from './tools.json'
+import toolTypeJA from './toolType.json'
+import trayJA from './tray.json'
+import updateJA from './update.json'
+import utilJA from './util.json'
+import versionmanagerJA from './versionmanager.json'
+
+export default {
+  ja: {
+    ai: aiJA,
+    apache: apacheJA,
+    appLog: appLogJA,
+    aside: asideJA,
+    base: baseJA,
+    conf: confJA,
+    feedback: feedbackJA,
+    fork: forkJA,
+    host: hostJA,
+    licenses: licensesJA,
+    mailpit: mailpitJA,
+    meilisearch: meilisearchJA,
+    menu: menuJA,
+    minio: minioJA,
+    mysql: mysqlJA,
+    nginx: nginxJA,
+    nodejs: nodejsJA,
+    ollama: ollamaJA,
+    php: phpJA,
+    prompt: promptJA,
+    redis: redisJA,
+    requestTimer: requestTimerJA,
+    service: serviceJA,
+    setup: setupJA,
+    'token-generator': tokenGeneratorJA,
+    tools: toolsJA,
+    toolType: toolTypeJA,
+    tray: trayJA,
+    update: updateJA,
+    util: utilJA,
+    versionmanager: versionmanagerJA
+  }
+}
