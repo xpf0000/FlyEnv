73aec6ed7975bbff7a42940d3731944540566c6b
徐鹏飞 <250881478@qq.com>
2025-07-09 22:53:52 +0800

    1. Add Bun module 2. Add Deno module 3. Add etcd module 4. Optimize MySQL service shutdown method to prevent data corruption and startup failures 5. Optimize MariaDB service shutdown method to prevent data corruption and startup failures 6. Add database management features for MySQL and MariaDB including: root password reset, database/user creation, and database backup

--- src/render/util/path-browserify.ts
+++ src/render/util/path-browserify.ts
@@ -1,4 +1,39 @@
 import _path from 'path-browserify'
 const { join, basename, dirname, extname, resolve } = _path
 
-export { join, basename, dirname, extname, resolve }
+function pathFixedToUnix(path: string) {
+  return path
+    .split('\\')
+    .filter((s) => !!s.trim())
+    .join('/')
+}
+
+const joinFixed = (...args: string[]) => {
+  const arr = args.map((a) => pathFixedToUnix(a))
+  return join(...arr)
+}
+
+const basenameFixed = (path: string, ext?: string) => {
+  return basename(pathFixedToUnix(path), ext)
+}
+
+const dirnameFixed = (path: string) => {
+  return dirname(pathFixedToUnix(path))
+}
+
+const extnameFixed = (path: string) => {
+  return extname(pathFixedToUnix(path))
+}
+
+const resolveFixed = (...args: string[]) => {
+  const arr = args.map((a) => pathFixedToUnix(a))
+  return resolve(...arr)
+}
+
+export {
+  joinFixed as join,
+  basenameFixed as basename,
+  dirnameFixed as dirname,
+  extnameFixed as extname,
+  resolveFixed as resolve
+}
