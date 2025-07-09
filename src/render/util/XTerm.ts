3de1cea71dd3df7789e3af687d9250a103dc4a51
徐鹏飞 <250881478@qq.com>
2025-07-09 23:02:59 +0800

    1. Add Bun module 2. Add Deno module 3. Add etcd module 4. Optimize MySQL service shutdown method to prevent data corruption and startup failures 5. Optimize MariaDB service shutdown method to prevent data corruption and startup failures 6. Add database management features for MySQL and MariaDB including: root password reset, database/user creation, and database backup

--- src/render/util/XTerm.ts
+++ src/render/util/XTerm.ts
@@ -216,8 +216,13 @@ class XTerm implements XTermType {
     return new Promise((resolve) => {
       this.resolve = resolve
       const param = [...cammand]
-      param.push(`wait;`)
-      param.push(`echo "Task-${this.ptyKey}-END" && exit 0;`)
+      if (window.Server.isWindows) {
+        param.push(`echo "Task-${this.ptyKey}-End"`)
+        param.push(`exit 0`)
+      } else {
+        param.push(`wait;`)
+        param.push(`echo "Task-${this.ptyKey}-END" && exit 0;`)
+      }
       IPC.send('NodePty:exec', this.ptyKey, param).then((key: string) => {
         console.log('static cammand finished: ', cammand)
         IPC.off(key)
