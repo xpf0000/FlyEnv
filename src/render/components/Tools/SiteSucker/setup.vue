21801b289f855cf5c61d090fd79aebe452c93563
徐鹏飞 <250881478@qq.com>
2025-07-10 00:00:15 +0800

    1. Add Bun module 2. Add Deno module 3. Add etcd module 4. Optimize MySQL service shutdown method to prevent data corruption and startup failures 5. Optimize MariaDB service shutdown method to prevent data corruption and startup failures 6. Add database management features for MySQL and MariaDB including: root password reset, database/user creation, and database backup

--- src/render/components/Tools/SiteSucker/setup.vue
+++ src/render/components/Tools/SiteSucker/setup.vue
@@ -18,101 +18,103 @@
             $t('base.confirm')
           }}</el-button>
         </div>
-        <div class="main-wapper p-3">
-          <div class="main p-5">
-            <el-form label-position="top" class="site-sucker-setup" @submit.prevent>
-              <el-form-item :label="$t('util.savePath')">
-                <div class="path-choose w-full">
-                  <input
-                    type="text"
-                    class="input"
-                    :placeholder="$t('util.saveAs')"
-                    readonly="true"
-                    :value="form.dir"
-                  />
-                  <div class="icon-block" @click="chooseRoot()">
-                    <yb-icon
-                      :svg="import('@/svg/folder.svg?raw')"
-                      class="choose"
-                      width="18"
-                      height="18"
+        <el-scrollbar class="flex-1">
+          <div class="main-wapper p-3">
+            <div class="main p-5">
+              <el-form label-position="top" class="site-sucker-setup" @submit.prevent>
+                <el-form-item :label="$t('util.savePath')">
+                  <div class="path-choose w-full">
+                    <input
+                      type="text"
+                      class="input"
+                      :placeholder="$t('util.saveAs')"
+                      readonly="true"
+                      :value="form.dir"
+                    />
+                    <div class="icon-block" @click="chooseRoot()">
+                      <yb-icon
+                        :svg="import('@/svg/folder.svg?raw')"
+                        class="choose"
+                        width="18"
+                        height="18"
+                      />
+                    </div>
+                  </div>
+                </el-form-item>
+                <el-form-item :label="$t('tools.windowCount')">
+                  <div class="path-choose w-full">
+                    <el-slider
+                      v-model.number="form.windowCount"
+                      :min="1"
+                      :max="CPU_Count"
+                      show-input
+                    ></el-slider>
+                  </div>
+                </el-form-item>
+                <el-form-item :label="$t('util.proxy')">
+                  <div class="path-choose w-full">
+                    <input
+                      v-model.trim="form.proxy"
+                      type="text"
+                      class="input"
+                      placeholder="Example: http://127.0.0.1:1087"
                     />
                   </div>
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('tools.windowCount')">
-                <div class="path-choose w-full">
-                  <el-slider
-                    v-model.number="form.windowCount"
-                    :min="1"
-                    :max="CPU_Count"
-                    show-input
-                  ></el-slider>
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('util.proxy')">
-                <div class="path-choose w-full">
-                  <input
-                    v-model.trim="form.proxy"
+                </el-form-item>
+                <el-form-item :label="$t('util.timeout')">
+                  <div class="path-choose w-full">
+                    <input
+                      v-model.number="form.timeout"
+                      type="number"
+                      class="input"
+                      :placeholder="$t('util.timeoutTips')"
+                    />
+                  </div>
+                </el-form-item>
+                <el-form-item :label="$t('util.maxImgSize')">
+                  <div class="path-choose w-full">
+                    <input
+                      v-model.number="form.maxImgSize"
+                      type="number"
+                      class="input"
+                      :placeholder="$t('util.maxImgSize')"
+                    />
+                    <div class="util-m">M</div>
+                  </div>
+                </el-form-item>
+                <el-form-item :label="$t('util.maxVideoSize')">
+                  <div class="path-choose w-full">
+                    <input
+                      v-model.number="form.maxVideoSize"
+                      type="number"
+                      class="input"
+                      :placeholder="$t('util.maxVideoSize')"
+                    />
+                    <div class="util-m">M</div>
+                  </div>
+                </el-form-item>
+                <el-form-item :label="$t('util.pageLimit')">
+                  <div class="path-choose w-full">
+                    <input
+                      v-model.trim="form.pageLimit"
+                      type="text"
+                      class="input"
+                      :placeholder="$t('util.pageLimitTips')"
+                    />
+                  </div>
+                </el-form-item>
+                <el-form-item :label="$t('util.LinkExclusion')">
+                  <textarea
+                    v-model.trim="form.excludeLink"
                     type="text"
-                    class="input"
-                    placeholder="Example: http://127.0.0.1:1087"
-                  />
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('util.timeout')">
-                <div class="path-choose w-full">
-                  <input
-                    v-model.number="form.timeout"
-                    type="number"
-                    class="input"
-                    :placeholder="$t('util.timeoutTips')"
-                  />
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('util.maxImgSize')">
-                <div class="path-choose w-full">
-                  <input
-                    v-model.number="form.maxImgSize"
-                    type="number"
-                    class="input"
-                    :placeholder="$t('util.maxImgSize')"
-                  />
-                  <div class="util-m">M</div>
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('util.maxVideoSize')">
-                <div class="path-choose w-full">
-                  <input
-                    v-model.number="form.maxVideoSize"
-                    type="number"
-                    class="input"
-                    :placeholder="$t('util.maxVideoSize')"
-                  />
-                  <div class="util-m">M</div>
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('util.pageLimit')">
-                <div class="path-choose w-full">
-                  <input
-                    v-model.trim="form.pageLimit"
-                    type="text"
-                    class="input"
-                    :placeholder="$t('util.pageLimitTips')"
-                  />
-                </div>
-              </el-form-item>
-              <el-form-item :label="$t('util.LinkExclusion')">
-                <textarea
-                  v-model.trim="form.excludeLink"
-                  type="text"
-                  class="input-textarea w-full"
-                  :placeholder="$t('util.LinkExclusionTips')"
-                ></textarea>
-              </el-form-item>
-            </el-form>
+                    class="input-textarea w-full"
+                    :placeholder="$t('util.LinkExclusionTips')"
+                  ></textarea>
+                </el-form-item>
+              </el-form>
+            </div>
           </div>
-        </div>
+        </el-scrollbar>
       </div>
     </template>
   </el-drawer>
