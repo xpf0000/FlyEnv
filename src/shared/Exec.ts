2357b7da5c9f0db3a6b4372ccf339188f5039a17
徐鹏飞 <250881478@qq.com>
2025-07-23 21:35:35 +0800

    Update Linux Support

--- src/shared/Exec.ts
+++ src/shared/Exec.ts
@@ -1,65 +1,70 @@
 import { basename, join } from 'path'
-import { chmod, copyFile, execPromiseWithEnv, uuid, writeFile } from '../fork/Fn'
-import { execPromise } from '@shared/child-process'
-import { remove } from '@shared/fs-extra'
+import { remove, chmod, copyFile, writeFile } from '@shared/fs-extra'
 import { isLinux, isMacOS, isWindows } from '@shared/utils'
+import { execa } from 'execa'
+import { waitTime, uuid } from '@/util/Index'
 
 type ExecType = {
+  escapeCommand: (command: string) => string
   runInTerminal: (command: string) => Promise<void>
 }
 
-function escapeShellArg(arg: string) {
+function escapeCommand(command: string) {
   // First check if argument needs wrapping
-  if (!arg.match(/[ "'\r\n]/)) return arg
+  if (!command.match(/[ "'\r\n]/)) return command
   // Escape double quotes by backslash-escaping them, then wrap in double quotes
-  return arg.replace(/'/g, `\\'`)
+  return command.replace(/"/g, '\\"')
 }
 
 const runInTerminal = async (c: string) => {
-  const command = escapeShellArg(c)
+  const command = escapeCommand(c)
   if (isMacOS()) {
-    const appleScript = `
-        tell application "Terminal"
-          if not running then
-            activate
-            do script '${command}' in front window
-          else
-            activate
-            do script '${command}'
-          end if
-        end tell`
+    const appleScript = `tell application "Terminal"
+  if not running then
+    activate
+    do script "${command}" in front window
+  else
+    activate
+    do script "${command}"
+  end if
+end tell`
     const scptFile = join(global.Server.Cache!, `${uuid()}.scpt`)
-    await writeFile(scptFile, appleScript)
-    await chmod(scptFile, '0777')
+    await writeFile(scptFile, appleScript.trim())
+    await chmod(scptFile, '0755')
     try {
-      await execPromise(`osascript ./${basename(scptFile)}`, {
+      await execa('osascript', [`./${basename(scptFile)}`], {
         cwd: global.Server.Cache!
       })
     } catch (e) {
       console.log('runInTerminal error', e)
     } finally {
-      await remove(scptFile)
+      await waitTime(300)
+      remove(scptFile).catch()
     }
     return
   }
   if (isLinux()) {
     const terminalSH = join(global.Server.Static!, 'sh/exec-by-terminal.sh')
-    const exeSH = join(global.Server.Cache!, `exec-by-terminal.sh`)
+    const exeSH = join(global.Server.Cache!, `${uuid()}.sh`)
     await copyFile(terminalSH, exeSH)
     await chmod(exeSH, '0755')
 
     try {
-      await execPromise(`"${exeSH}" '${command}'`, {
+      await execa(basename(exeSH), [command], {
         cwd: global.Server.Cache!
       })
     } catch (e) {
       console.log('runInTerminal error', e)
+    } finally {
+      await waitTime(300)
+      remove(exeSH).catch()
     }
+    return
   }
 
   if (isWindows()) {
     try {
-      await execPromiseWithEnv(`start powershell -NoExit -Command '${command}'`)
+      await execa('start', ['powershell', '-NoExit', '-Command', `& {${command}}`])
     } catch (e) {
       console.log('runInTerminal error', e)
     }
@@ -67,5 +72,6 @@ const runInTerminal = async (c: string) => {
 }
 
 export const ExecCommand: ExecType = {
+  escapeCommand,
   runInTerminal
 }
