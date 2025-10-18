2a72ed0ff104baf002bb8f131dcb631396e4fc49
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2025-10-18 15:51:50 +0200

    add German localization

---
+++ src/lang/de/index.ts
@@ -0,0 +1,69 @@
+import aiDE from './ai.json'
+import apacheDE from './apache.json'
+import appLogDE from './appLog.json'
+import asideDE from './aside.json'
+import baseDE from './base.json'
+import confDE from './conf.json'
+import feedbackDE from './feedback.json'
+import forkDE from './fork.json'
+import hostDE from './host.json'
+import mailpitDE from './mailpit.json'
+import menuDE from './menu.json'
+import mysqlDE from './mysql.json'
+import nginxDE from './nginx.json'
+import nodejsDE from './nodejs.json'
+import ollamaDE from './ollama.json'
+import phpDE from './php.json'
+import promptDE from './prompt.json'
+import redisDE from './redis.json'
+import serviceDE from './service.json'
+import setupDE from './setup.json'
+import tokenGeneratorDE from './token-generator.json'
+import toolsDE from './tools.json'
+import toolTypeDE from './toolType.json'
+import trayDE from './tray.json'
+import updateDE from './update.json'
+import utilDE from './util.json'
+import versionmanagerDE from './versionmanager.json'
+import licensesDE from './licenses.json'
+import requestTimer from './requestTimer.json'
+import meilisearch from './meilisearch.json'
+import minio from './minio.json'
+import podman from './podman.json'
+
+export default {
+  en: {
+    podman,
+    minio,
+    meilisearch,
+    requestTimer,
+    licenses: licensesDE,
+    ai: aiDE,
+    apache: apacheDE,
+    appLog: appLogDE,
+    aside: asideDE,
+    base: baseDE,
+    conf: confDE,
+    feedback: feedbackDE,
+    fork: forkDE,
+    host: hostDE,
+    mailpit: mailpitDE,
+    menu: menuDE,
+    mysql: mysqlDE,
+    nginx: nginxDE,
+    nodejs: nodejsDE,
+    ollama: ollamaDE,
+    php: phpDE,
+    prompt: promptDE,
+    redis: redisDE,
+    service: serviceDE,
+    setup: setupDE,
+    'token-generator': tokenGeneratorDE,
+    tools: toolsDE,
+    toolType: toolTypeDE,
+    tray: trayDE,
+    update: updateDE,
+    util: utilDE,
+    versionmanager: versionmanagerDE
+  }
+}
