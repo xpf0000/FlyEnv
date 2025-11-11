6238c309a083f98bd8f764d6715949533164a8f0
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2025-11-11 13:17:00 +0100

    Rename lang code for Ukrainian to 'uk'

---
+++ src/lang/uk/index.ts
@@ -0,0 +1,69 @@
+import aiUK from './ai.json'
+import apacheUK from './apache.json'
+import appLogUK from './appLog.json'
+import asideUK from './aside.json'
+import baseUK from './base.json'
+import confUK from './conf.json'
+import feedbackUK from './feedback.json'
+import forkUK from './fork.json'
+import hostUK from './host.json'
+import mailpitUK from './mailpit.json'
+import menuUK from './menu.json'
+import mysqlUK from './mysql.json'
+import nginxUK from './nginx.json'
+import nodejsUK from './nodejs.json'
+import ollamaUK from './ollama.json'
+import phpUK from './php.json'
+import promptUK from './prompt.json'
+import redisUK from './redis.json'
+import serviceUK from './service.json'
+import setupUK from './setup.json'
+import tokenGeneratorUK from './token-generator.json'
+import toolsUK from './tools.json'
+import toolTypeUK from './toolType.json'
+import trayUK from './tray.json'
+import updateUK from './update.json'
+import utilUK from './util.json'
+import versionmanagerUK from './versionmanager.json'
+import licensesUK from './licenses.json'
+import requestTimer from './requestTimer.json'
+import meilisearch from './meilisearch.json'
+import minio from './minio.json'
+import podman from './podman.json'
+
+export default {
+  uk: {
+    podman,
+    minio,
+    meilisearch,
+    requestTimer,
+    licenses: licensesUK,
+    ai: aiUK,
+    apache: apacheUK,
+    appLog: appLogUK,
+    aside: asideUK,
+    base: baseUK,
+    conf: confUK,
+    feedback: feedbackUK,
+    fork: forkUK,
+    host: hostUK,
+    mailpit: mailpitUK,
+    menu: menuUK,
+    mysql: mysqlUK,
+    nginx: nginxUK,
+    nodejs: nodejsUK,
+    ollama: ollamaUK,
+    php: phpUK,
+    prompt: promptUK,
+    redis: redisUK,
+    service: serviceUK,
+    setup: setupUK,
+    'token-generator': tokenGeneratorUK,
+    tools: toolsUK,
+    toolType: toolTypeUK,
+    tray: trayUK,
+    update: updateUK,
+    util: utilUK,
+    versionmanager: versionmanagerUK
+  }
+}
