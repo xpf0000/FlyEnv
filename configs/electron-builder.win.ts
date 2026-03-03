147880ab856a1765fdca449630b21f4d08a71481
xupengfei <250881478@qq.com>
2026-03-03 15:01:31 +0800

    1. Add Cloudflare Tunnel Module 2. Add Cloudflared Module 3. Python And Go add new project creation features. 4. Fixed DNS ip address issues

--- configs/electron-builder.win.ts
+++ configs/electron-builder.win.ts
@@ -4,7 +4,7 @@ import AfterSign from '../build/afterSign'
 const conf: Configuration = {
   productName: 'FlyEnv',
   executableName: 'FlyEnv',
-  buildVersion: '4.13.2',
+  buildVersion: '4.13.3',
   electronVersion: '35.7.5',
   appId: 'phpstudy.xpfme.com',
   asar: true,
