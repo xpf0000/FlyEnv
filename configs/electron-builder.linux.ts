3d5004c5b114bc1adcd790d64bdb8daba9f8307f
xupengfei <250881478@qq.com>
2026-03-21 10:02:09 +0800

    1. Added n8n Module. 2. OpenClaw Added Config Tab

--- configs/electron-builder.linux.ts
+++ configs/electron-builder.linux.ts
@@ -15,7 +15,7 @@ const desktop: any = {
 const conf: Configuration = {
   productName: 'FlyEnv',
   executableName: 'FlyEnv',
-  buildVersion: '4.13.5',
+  buildVersion: '4.13.6',
   electronVersion: '35.7.5',
   appId: 'com.xpf0000.flyenv',
   asar: true,
