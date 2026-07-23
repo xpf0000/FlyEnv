81b8dcb19b990d4da0c11315a002c3ab14895c7f
xupengfei <250881478@qq.com>
2026-07-23 15:32:25 +0800

    feat: add ClickHouse render components and icon

---
+++ src/render/components/ClickHouse/Logs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="module-config">
+    <el-card>
+      <LogVM ref="log" :log-file="filepath" />
+      <template #footer>
+        <ToolVM :log="log" />
+      </template>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed, ref } from 'vue'
+  import LogVM from '@/components/Log/index.vue'
+  import ToolVM from '@/components/Log/tool.vue'
+  import { join } from '@/util/path-browserify'
+
+  const log = ref()
+  const filepath = computed(() => {
+    return join(window.Server.ClickHouseDir!, 'log/server.log')
+  })
+</script>
