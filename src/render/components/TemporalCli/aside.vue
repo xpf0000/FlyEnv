b1c099ba59efff23dcc6d36a3f434a43f9e87429
xupengfei <250881478@qq.com>
2026-08-04 10:58:02 +0800

    Fix Issues

--- src/render/components/TemporalCli/aside.vue
+++ src/render/components/TemporalCli/aside.vue
@@ -7,7 +7,7 @@
     <div class="left">
       <div class="icon-block" :class="{ run: serviceRunning }">
         <yb-icon
-          :svg="import('@/svg/temporal-cli.svg?raw')"
+          :svg="import('@/svg/temporal.svg?raw')"
           style="padding: 5px"
           width="28"
           height="28"
