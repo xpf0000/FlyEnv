74c90bb3dcbe42d95081b688372a4980a163a5a2
Le_Sage <le_sage@live.com>
2026-08-14 04:43:34 +0200

    Add descriptions for optional service modules

--- src/render/components/Setup/ModuleShowHide/index.vue
+++ src/render/components/Setup/ModuleShowHide/index.vue
@@ -3,7 +3,16 @@
     class="relative flex justify-center items-center py-5 bg-[#33445526] w-full rounded-md dark:bg-[#32364A]"
   >
     <div class="flex items-center">
-      <div class="w-[130px] truncate">{{ title }}</div>
+      <div class="w-[130px] flex items-center gap-1">
+        <div class="truncate">{{ title }}</div>
+        <el-tooltip
+          v-if="description"
+          :content="description"
+          placement="top"
+        >
+          <InfoFilled class="w-[14px] h-[14px] shrink-0 cursor-help text-zinc-400" />
+        </el-tooltip>
+      </div>
       <el-switch v-model="showItem" :loading="changing" :disabled="changing" />
     </div>
     <slot name="default"></slot>
@@ -16,6 +25,8 @@
   import { AppCustomerModule } from '@/core/Module'
   import { canSetModuleVisibility } from '@/core/ModuleVisibility'
   import type { AllAppModule } from '@/core/type'
+  import { InfoFilled } from '@element-plus/icons-vue'
+  import { I18nT } from '@lang/index'
 
   type StringFn = () => string
 
@@ -29,6 +40,19 @@
       return props.label
     }
     return props.label?.() ?? ''
+  })
+
+  const descriptionKeys: Record<string, string> = {
+    consul: 'setup.moduleDescription.consul',
+    etcd: 'setup.moduleDescription.etcd',
+    rnacos: 'setup.moduleDescription.rnacos',
+    temporal: 'setup.moduleDescription.temporal',
+    'temporal-cli': 'setup.moduleDescription.temporalCli'
+  }
+
+  const description = computed(() => {
+    const key = descriptionKeys[props.typeFlag]
+    return key ? I18nT(key) : ''
   })
 
   const appStore = AppStore()
