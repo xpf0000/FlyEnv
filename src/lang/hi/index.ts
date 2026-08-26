2e4750af071484053e1f6bded31d8804ad433367
Md. Nayan Chowdhury <mdnayanchowdhury9@gmail.com>
2026-08-26 18:12:21 +0600

    Add files via upload
    Added Hindi translation

---
+++ src/lang/hi/index.ts
@@ -0,0 +1,98 @@
+import aiHI from './ai.json'
+import apacheHI from './apache.json'
+import appLogHI from './appLog.json'
+import asideHI from './aside.json'
+import baseHI from './base.json'
+import confHI from './conf.json'
+import feedbackHI from './feedback.json'
+import forkHI from './fork.json'
+import hostHI from './host.json'
+import mailpitHI from './mailpit.json'
+import menuHI from './menu.json'
+import mysqlHI from './mysql.json'
+import nginxHI from './nginx.json'
+import nodejsHI from './nodejs.json'
+import ollamaHI from './ollama.json'
+import phpHI from './php.json'
+import promptHI from './prompt.json'
+import redisHI from './redis.json'
+import serviceHI from './service.json'
+import setupHI from './setup.json'
+import tokenGeneratorHI from './token-generator.json'
+import toolsHI from './tools.json'
+import toolTypeHI from './toolType.json'
+import trayHI from './tray.json'
+import updateHI from './update.json'
+import utilHI from './util.json'
+import versionmanagerHI from './versionmanager.json'
+import licensesHI from './licenses.json'
+import requestTimer from './requestTimer.json'
+import meilisearch from './meilisearch.json'
+import minio from './minio.json'
+import podman from './podman.json'
+import openclaw from './openclaw.json'
+import n8n from './n8n.json'
+import rustfs from './rustfs.json'
+import mkcert from './mkcert.json'
+import hermes from './hermes.json'
+import flutter from './flutter.json'
+import cron from './cron.json'
+import kimi from './kimi.json'
+import claudeCode from './claude-code.json'
+import codex from './codex.json'
+import openCode from './opencode.json'
+import antigravity from './antigravity.json'
+import copilotCli from './copilot-cli.json'
+import common from './common.json'
+import mcp from './mcp.json'
+export default {
+  hi: {
+    common,
+    kimi,
+    claudeCode,
+    codex,
+    openCode,
+    antigravity,
+    copilotCli,
+    mcp,
+    cron,
+    rustfs,
+    mkcert,
+    flutter,
+    hermes,
+    n8n,
+    openclaw,
+    podman,
+    minio,
+    meilisearch,
+    requestTimer,
+    licenses: licensesHI,
+    ai: aiHI,
+    apache: apacheHI,
+    appLog: appLogHI,
+    aside: asideHI,
+    base: baseHI,
+    conf: confHI,
+    feedback: feedbackHI,
+    fork: forkHI,
+    host: hostHI,
+    mailpit: mailpitHI,
+    menu: menuHI,
+    mysql: mysqlHI,
+    nginx: nginxHI,
+    nodejs: nodejsHI,
+    ollama: ollamaHI,
+    php: phpHI,
+    prompt: promptHI,
+    redis: redisHI,
+    service: serviceHI,
+    setup: setupHI,
+    'token-generator': tokenGeneratorHI,
+    tools: toolsHI,
+    toolType: toolTypeHI,
+    tray: trayHI,
+    update: updateHI,
+    util: utilHI,
+    versionmanager: versionmanagerHI
+  }
+}
