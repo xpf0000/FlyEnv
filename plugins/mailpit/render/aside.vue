1551d542a9fe80656e76c033b4c8a8d44bc44fa2
徐鹏飞 <250881478@qq.com>
2026-09-20 15:52:36 +0800

    plugin: add real Mailpit example plugins/mailpit/render/aside.vue

---
+++ plugins/mailpit/render/aside.vue
@@ -0,0 +1,28 @@
+<template>
+  <li :class="{ active: currentPath === '/mailpit-plugin' }" @click="nav">
+    <div class="left">
+      <div class="icon-block">
+        <yb-icon
+          :svg="import('@/svg/mailpit.svg?raw')"
+          style="padding: 5px"
+          width="28"
+          height="28"
+        />
+      </div>
+      <span class="title">Mailpit Plugin</span>
+    </div>
+  </li>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { useRoute, useRouter } from 'vue-router'
+
+  const route = useRoute()
+  const router = useRouter()
+  const currentPath = computed(() => route.path)
+
+  const nav = () => {
+    router.push('/mailpit-plugin').catch(() => {})
+  }
+</script>
