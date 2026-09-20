74e2ab08df7f2c2988e7b7c455c6c5e716b6ac65
徐鹏飞 <250881478@qq.com>
2026-09-20 15:52:33 +0800

    plugin: add real Mailpit example plugins/mailpit/fork/index.ts

---
+++ plugins/mailpit/fork/index.ts
@@ -0,0 +1,5 @@
+// Reuse FlyEnv's real Mailpit backend implementation.
+// plugin:build bundles the referenced FlyEnv fork sources into the standalone plugin artifact.
+import MailPit from '@fork/module/MailPit'
+
+export default MailPit
