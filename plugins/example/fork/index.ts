55a006e22db02a323817a73da300a41d73fc2198
徐鹏飞 <250881478@qq.com>
2026-09-20 15:41:00 +0800

    plugin: make example fork compatible with FlyEnv lifecycle

--- plugins/example/fork/index.ts
+++ plugins/example/fork/index.ts
@@ -1,12 +1,17 @@
+import { ForkPromise } from '@shared/ForkPromise'
+
 class ExamplePluginFork {
   init() {}
 
   exec(fn: string, ...args: any[]) {
-    const handler = (this as any)[fn]
-    if (typeof handler !== 'function') {
-      return Promise.reject(new Error(`Unknown example plugin command: ${fn}`))
-    }
-    return Promise.resolve(handler.apply(this, args))
+    return new ForkPromise<any>((resolve, reject) => {
+      const handler = (this as any)[fn]
+      if (typeof handler !== 'function') {
+        reject(new Error(`Unknown example plugin command: ${fn}`))
+        return
+      }
+      Promise.resolve(handler.apply(this, args)).then(resolve).catch(reject)
+    })
   }
 
   ping(value = 'pong') {
